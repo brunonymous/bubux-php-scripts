de361e037942a4d0b7fef650ceec701736a51b80
bruno.ethvignot <bruno.ethvignot@gmail.com>
2013-06-23 14:02:15 +0000

    update text and URL

--- amigatlk/modules/amigatlk/controllers/default.classic.php
+++ amigatlk/modules/amigatlk/controllers/default.classic.php
@@ -4,7 +4,8 @@
 * @subpackage amigatlk
 * @author     Bruno Ethvignot <bruno at tlk dot biz> 
 * @copyright  2013 TLK Games
-* @date       2013-06-10
+* @created    2013-04-01
+* @date       2013-06-23
 * @link       http://amiga.tlk.fr/
 * @license    GNU GPL v3
 */
@@ -14,16 +15,23 @@ class defaultCtrl extends jController {
      *
      */
     function index() {
-        $rep = $this->getResponse('html');
-        $lang = jLocale::getCurrentLang();
+        $lang     = jLocale::getCurrentLang();
+        if ( $lang != 'fr' ) {
+            $rep = $this->getResponse('redirect');
+            $rep->action = "amigatlk~default:index";
+            $rep->params = array('lang' => 'fr');
+            return $rep;
+        }
+        $rep      = $this->getResponse('html');
         $articles = jClasses::getService('amigatlk~articles');
-        $article = $articles->getArticle( 'homepage' );
-        if (empty ($article)) {
+        $article  = $articles->getArticle( 'homepage' );
+        if ( empty ($article) ) {
             $rep->body->assignZone( 'MAIN', 'amigatlk~notFound404' );
             $rep->setHttpStatus( '404', 'Not Found' );
             return $rep;
         }
-        $rep->body->assignZone('MAIN', 'amigatlk~viewArticle', array('article' => $article));
+        $rep->body->assignZone( 'MAIN', 'amigatlk~viewArticle', 
+            array( 'article' => $article ) );
         return $rep;
     }
 }
